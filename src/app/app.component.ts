f7d22df493c9d136fc10cb3362e163b092cdc391
roong <roongsetfire@gmail.com>
2021-02-25 15:42:01 +0700

    viewchild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styles: []
 })
-export class AppComponent {
-  title = 'Recipe1';
+export class AppComponent implements OnInit {
+ @ViewChild('productList',{ static: true })
+ productList: ProductListComponent;
+
+ ngOnInit(): void {
+   this.productList.products = [
+     {
+       name: 'ส้มโอ',
+       price: 111
+     },
+     {
+       name: 'แตงโม',
+       price: 222
+     },
+     {
+       name: 'มะพร้าวนํ้าหอม',
+       price: 333
+     }
+   ];
+ }
+ constructor() {}
 }
