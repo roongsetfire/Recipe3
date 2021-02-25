041d387f0ee235fb33b6072aa7762df68977f86a
roong <roongsetfire@gmail.com>
2021-02-25 15:27:15 +0700

    second commit

--- src/app/product-item/product-item.component.ts
+++ src/app/product-item/product-item.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Product } from '../product';
 
 @Component({
  selector: 'app-product-item',
- templateUrl: './product-item.component.html' ,
- styleUrls: ['./product-item.component.css' ]
+ templateUrl: './product-item.component.html',
+ styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
  @Input()
  product: Product;
+
+ @Output()
+ OnSelected: EventEmitter<Product> = new EventEmitter();
+
  constructor() {}
 
  ngOnInit() {}
+
+ select() {
+   this.OnSelected.emit(this.product);
+ }
 }
